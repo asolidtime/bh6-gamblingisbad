2e3b4cad8a3fedf02be1ead118ae81af4e5ad6a9
asolidtime <75963592+asolidtime@users.noreply.github.com>
2023-03-26 11:34:31 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,3 +5,11 @@ def draw_text_centered(text, x, y, size, color):
 
 def draw_text_really_centered(text, x, y, size, color):
     pr.draw_text(text, int(x - pr.measure_text(text, size) / 2), int(y - size/2), size, color)
+    
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
